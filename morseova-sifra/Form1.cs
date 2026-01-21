8b5c4f6ebc9f5d87981b0026e1aad6e0d85fed6d
hrnkovocesstvopcdole <BabnicA.itb25@vsps-su.cz>
2026-01-21 09:26:04 +0100

    added support for multiple characters at same time

--- morseova-sifra/Form1.cs
+++ morseova-sifra/Form1.cs
@@ -52,6 +52,7 @@ namespace morseova_sifra
                 {
                     index = Array.IndexOf(morseova, Convert.ToString(textTODECODE));
                     textt += Convert.ToString(abeceda.ElementAt(index));
+                    //textTODECODE = "";
                 }
                 else { textTODECODE += textEncoded.Text[i]; };
 
